bfe52ab1eebfe43014d6db44444f728bd1c54ab3
Eric Liu <ericliu404@gmail.com>
2016-05-21 15:04:13 -0400

    Seperate text and background rotation

--- script.js
+++ script.js
@@ -7,20 +7,31 @@ var background = [
   {link: "url(images/background5.jpg)", color: "#FFFFFF", text: "Academically Qualified"},
   {link: "url(images/background6.jpg)", color: "#FFFFFF", text: "Efficient Worker"},
 ];
+var i = 0;
+var j = 0;
 
 //Change background image to random choice every 7s
-document.getElementById("banner").style.background = background[0].link + " center / cover";
+document.getElementById("banner").style.background = background[j].link + " center / cover";
 setInterval(function() {
-	document.getElementById("banner").style.background = background[1].link + " center / cover";
-	document.getElementById("info").style.color = background[1].color;
-	document.getElementById("info").style.opacity = "0";
-	setTimeout(function() {
-		document.getElementById("info").innerHTML = background[0].text;
-		document.getElementById("info").style.opacity = "1";
-	}, 1000);
-	background.push(background.shift());
+	document.getElementById("banner").style.background = background[j].link + " center / cover";
+	document.getElementById("info").style.color = background[j].color;
+	j++;
+	if (j == 7) {
+		j = 0;
+	}
 }, 7000);
 
+setInterval(function() {
+	document.getElementById("info").style.opacity = "0";
+	setTimeout(function() {
+		document.getElementById("info").innerHTML = background[i].text;
+		document.getElementById("info").style.opacity = "1";
+	}, 500);
+	i++;
+	if (i == 7) {
+		i = 0;
+	}
+}, 3000);
 
 $('a').click(function(){
     $('html, body').animate({
