81005bfabe87b5fcf48815245f746b6f93090c67
Eric Liu <Eric Liu>
2016-05-21 12:12:58 -0400

    Optimize images and change image randomizer

--- script.js
+++ script.js
@@ -4,16 +4,18 @@ var background = [
   {link: "url(images/background2.jpg)", color: "#FFFFFF", text: "Quick Learner"},
   {link: "url(images/background3.jpg)", color: "#FFFFFF", text: "Individualistic"},
   {link: "url(images/background4.jpeg)", color: "#FFFFFF", text: "Disciplined"}, 
-  {link: "url(images/background5.jpeg)", color: "#FFFFFF", text: "Academically Qualified"},
+  {link: "url(images/background5.jpg)", color: "#FFFFFF", text: "Academically Qualified"},
+  {link: "url(images/background6.jpg)", color: "#FFFFFF", text: "Efficient Worker"},
 ];
 
 //Change background image to random choice every 7s
+document.getElementById("banner").style.background = background[0].link + " center / cover";
 setInterval(function() {
 	document.getElementById("banner").style.background = background[1].link + " center / cover";
 	document.getElementById("info").style.color = background[1].color;
 	document.getElementById("info").style.opacity = "0";
 	setTimeout(function() {
-		document.getElementById("info").innerHTML = background[1].text;
+		document.getElementById("info").innerHTML = background[0].text;
 		document.getElementById("info").style.opacity = "1";
 	}, 1000);
 	background.push(background.shift());
