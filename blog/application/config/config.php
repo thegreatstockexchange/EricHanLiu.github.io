ff9e0c4d1e78c558eb87ab657c059209ea38a280
Eric Liu <ericliu404@gmail.com>
2016-06-10 22:42:17 -0400

    Change basepath

--- blog/application/config/config.php
+++ blog/application/config/config.php
@@ -22,7 +22,7 @@
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://erichanliu.github.io/blog';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
