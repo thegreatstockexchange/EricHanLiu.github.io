1afb5a7c19275b284e93175212206e66e3b0fa2e
Eric Liu <ericliu404@gmail.com>
2016-06-13 18:42:22 -0400

    Revert previous changes

---
+++ 1/script.js
@@ -0,0 +1,65 @@
+var background = [
+  {link: "url(css/background.jpg)", color: "#894264"},
+  {link: "url(css/background1.jpg)", color: "#93B6CC"},
+  {link: "url(css/background2.jpg)", color: "#9FAFBF"},
+  {link: "url(css/background3.jpg)", color: "#916852"},
+  {link: "url(css/background4.jpeg)", color: "#6D8893"}, 
+  {link: "url(css/background5.jpeg)", color: "#8A8A8A"},
+  {link: "url(css/background6.jpeg)", color: "#636363"},
+  {link: "url(css/background7.jpeg)", color: "#BBCAD0"},
+];
+
+//Change background image to random choice every 7s
+setInterval(function() {
+  var chosenImage = Math.floor(Math.random() * 8);
+  document.body.style.backgroundImage = background[chosenImage].link;
+  document.getElementById("name").style.color = background[chosenImage].color;
+  document.getElementsByClassName("border")[0].style.borderColor = background[chosenImage].color;
+  document.getElementsByClassName("border")[1].style.borderColor = background[chosenImage].color;
+}, 7000);
+
+
+
+//CODE FOR PAGE CHANGE AND TEXT TRANSITION
+function navClear() {
+    document.querySelector("#aboutLink").style.color = "#FFFFFF";
+    document.querySelector("#homeLink").style.color = "#FFFFFF";
+}
+
+function navColor(link) {
+    document.querySelector(link).style.color = "#9FAFBF";
+}
+
+//function to select the id for easier manipulation (don't have to get id for each)
+function $(id){
+    return document.getElementById(id); 
+}
+
+//default shows the home page 
+$('page-').classList.add('clicked');
+
+//function for page loading
+//(corresponding link with page, current page to clear, page to load) 
+function loadPage(link, pageRm, pageAdd) {
+    navClear();
+    navColor(link);
+    $(pageRm).classList.remove("clicked");
+    setTimeout(function() {
+        $(pageAdd).classList.add("clicked");
+    }, 250);
+};
+
+
+window.addEventListener("hashchange", function() {
+  var hash = window.location.hash;
+  hash = hash.slice(1); // Removes the # at the start of the hash to fit if statements
+    if (hash == "") {  
+        loadPage("#homeLink", "page-about", "page-");
+    } else if (hash == "about") {
+        loadPage("#aboutLink", "page-", "page-about");
+    }
+});
+
+//So the home page will load on default page load
+window.dispatchEvent(new Event("hashchange"));
+
---
+++ script.js
@@ -0,0 +1,72 @@
+var background = [
+  {link: "url(images/background.jpg)", color: "#FFFFFF"},
+  {link: "url(images/background1.jpg)", color: "#FFFFFF"},
+  {link: "url(images/background2.jpg)", color: "#FFFFFF"},
+  {link: "url(images/background3.jpg)", color: "#FFFFFF"},
+  {link: "url(images/background4.jpg)", color: "#FFFFFF"}, 
+  {link: "url(images/background5.jpg)", color: "#FFFFFF"},
+  {link: "url(images/background6.jpg)", color: "#FFFFFF"},
+];
+var text = [
+	"Distinguished Musician",
+	"Developing Programmer",
+	"Quick Learner",
+	"Efficient Worker",
+	"Disciplined",
+	"Academically Qualified",
+];
+
+//randomize background on page load
+shuffle(background);
+
+//Change background image to random choice every 7s
+document.getElementById("banner").style.background = background[0].link + " center / cover";
+setInterval(function() {
+	document.getElementById("banner").style.background = background[1].link + " center / cover";
+	//unused right now since all colours are white
+	document.getElementById("info").style.color = background[1].color;
+	background.push(background.shift());
+}, 7000);
+
+//Change text every 4 seconds
+document.getElementById("info").innerHTML = text[0];
+setInterval(function() {
+	document.getElementById("info").style.opacity = "0";
+	setTimeout(function() {
+		document.getElementById("info").innerHTML = text[0];
+		document.getElementById("info").style.opacity = "1";
+	}, 500);
+	text.push(text.shift());
+}, 5000);
+
+$("a").click(function(){
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 800);
+    return false;
+});
+
+window.onscroll = function () {  
+	var ele = document.getElementById("about");
+	var a = document.body.scrollTop;
+	if (a > "400") {
+		document.getElementById("back").style.opacity = "1";
+		document.getElementById("back").style.position = "fixed";
+		document.getElementById("back").style.pointerEvents = "auto";
+	}
+	else {
+		document.getElementById("back").style.opacity = "0";
+		document.getElementById("back").style.pointerEvents = "none";
+	}
+};
+
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length; i; i -= 1) {
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
+    }
+}
+
