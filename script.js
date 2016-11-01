a4ab52a9a9c81be1a236862ee8eadf543a444362
Eric Han Liu <ericliu404@gmail.com>
2016-11-01 10:58:27 -0400

    Remove obselete shuffle function

--- script.js
+++ script.js
@@ -64,13 +64,4 @@ window.onscroll = function () {
 	}
 };
 
-function shuffle(a) {
-    var j, x, i;
-    for (i = a.length; i; i -= 1) {
-        j = Math.floor(Math.random() * i);
-        x = a[i - 1];
-        a[i - 1] = a[j];
-        a[j] = x;
-    }
-}
 
